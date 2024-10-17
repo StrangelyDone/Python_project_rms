a538b78cde50d9966bfc13546235cfb1faa9b174
Abhi00728 <abhinavthumu007@gmail.com>
2024-10-17 12:17:24 +0530

    Implemented add feauture made couple of changes in customer UI

--- Customer_UI.py
+++ Customer_UI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
 from tkinter.scrolledtext import ScrolledText
-
+import Menu_Management
 class mine:
 
     def __init__(self):
@@ -12,7 +12,7 @@ class mine:
         self.root.attributes('-fullscreen', True)
         self.root.configure(bg='black')
 
-        self.image_path=PhotoImage(file=r'/Users/brindap/Downloads/open page 1 Large.png')
+        self.image_path=PhotoImage(file='images/open page 1 Large.png')
         self.bg_image=tk.Label(self.root, image=self.image_path)
         self.bg_image.pack()
 
@@ -64,25 +64,25 @@ class mine:
         Hazel Nutella Brownie,96    Chocolate Ball,30      Choco Walnut Brownie,60
         Choco Chip Brownie,60        Choco Chip Cake,540"""
 
-        self.image_1=PhotoImage(file=r'/Users/brindap/Downloads/starters Small.png')
+        self.image_1=PhotoImage(file='images/starters Small.png')
         
         self.button_starters = tk.Button(self.second_frame , text="Starters", font=('Arial', 18), image=self.image_1, command=self.starters)
         self.button_starters.grid(row=0, column=0, padx=100)
         self.button_starters.image=self.image_1
 
-        self.image_2=PhotoImage(file=r'/Users/brindap/Downloads/meals Small.png')
+        self.image_2=PhotoImage(file='images/meals Small.png')
 
         self.button_meals = tk.Button(self.second_frame , text="Meals", font=('Arial', 18),image=self.image_2, command=self.meals)
         self.button_meals.grid(row=0, column=2, padx=100)
         self.button_meals.image=self.image_2
 
-        self.image_3=PhotoImage(file=r'/Users/brindap/Downloads/desserts Small.png')
+        self.image_3=PhotoImage(file='images/desserts Small.png')
 
         self.button_desserts = tk.Button(self.second_frame , text="Desserts", font=('Arial', 18),image=self.image_3, command=self.desserts)
         self.button_desserts.grid(row=1, column=1)
         self.button_desserts.image=self.image_3
 
-        self.buttonframe.pack(fill='x')
+        #self.buttonframe.pack(fill='x')
 
         self.root.mainloop()
 
@@ -92,43 +92,54 @@ class mine:
         self.root.attributes('-fullscreen', True)
         self.root.title("Starters")
         self.root.configure(bg='black')
-        
-        self.check_state_1 = tk.IntVar()
-        self.check_state_2= tk.IntVar()
-        self.check_state_3 = tk.IntVar()
-        self.check_state_4 = tk.IntVar()
-        self.check_state_5 = tk.IntVar()
-        self.check_state_6 = tk.IntVar()
 
-        self.image_1=PhotoImage(file=r'/Users/brindap/Downloads/Veg Manchuria Small.png')
+        self.check_state_6 = tk.IntVar()
+        menu=Menu_Management.Menu_class()
+        
+        
+        starters = menu.get_starter_items()
+        variables = []
+
+
+        for i in range(len(starters)):
+            var = tk.IntVar
+            variables.append(var)
+            image1=PhotoImage(file=f'images/{starters[i]}.png')
+
+            self.check = tk.Checkbutton(self.root, text=f"{starters[i]}", font=('Arial', 16), bg='black', image=image1, variable=var)
+            self.check.grid(row=(i%3), column=(i//3))#removed padding cuz why not
+            self.check.image = image1
+
+
+        '''self.image_1=PhotoImage(file='Veg Manchuria Small.png')
 
         self.check_1 = tk.Checkbutton(self.root, text="Veg Manchuria 70", font=('Arial', 16), bg='black', image=self.image_1, variable=self.check_state_1)
         self.check_1.grid(row=0, column=0, padx=45, pady=40)
         self.check_1.image=self.image_1
 
-        self.image_2=PhotoImage(file=r'/Users/brindap/Downloads/Chicken Manchuria Small.png')
+        self.image_2=PhotoImage(file='Chicken Manchuria Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Chicken Manchuria 100", font=('Arial', 16), bg='black', image=self.image_2, variable=self.check_state_2)
         self.check.grid(row=0, column=1, padx=30, pady=40)
         self.check.image=self.image_2
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Fruit Salad Small.png')
+        self.image=PhotoImage(file='Fruit Salad Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Fruit Salad 50", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_3)
         self.check.grid(row=0, column=2, padx=45, pady=40)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Vegetable Salad Small.png')
+        self.image=PhotoImage(file='Vegetable Salad Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Vegetable Salad 30", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_4)
         self.check.grid(row=1, column=0, padx=90)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Fruit custard Small.png')
+        self.image=PhotoImage(file='Fruit custard Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Fruit Custard 50", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_5)
         self.check.grid(row=1, column=1, padx=90)
-        self.check.image=self.image
+        self.check.image=self.image'''
 
         self.check = tk.Checkbutton(self.root, text="Confirm selected dishes", font=('Arial', 16), bg='black', fg='white', variable=self.check_state_6)
         self.check.grid( pady=20)
@@ -148,50 +159,60 @@ class mine:
         self.root.attributes('-fullscreen', True)
         self.root.title("Starters")
         self.root.configure(bg='black')
-        
-        self.check_state_1 = tk.IntVar()
-        self.check_state_2= tk.IntVar()
-        self.check_state_3 = tk.IntVar()
-        self.check_state_4 = tk.IntVar()
-        self.check_state_5 = tk.IntVar()
-        self.check_state_6= tk.IntVar()
-        self.check_state_7=tk.IntVar()
 
-        self.image_1=PhotoImage(file=r'/Users/brindap/Downloads/Chicken Dum Biriyani Small.png')
+        self.check_state_7=tk.IntVar()
+        menu=Menu_Management.Menu_class()
+        
+        
+        meals= menu.get_meal_items()
+        variables = []
+
+
+        for i in range(len(meals)):
+            var = tk.IntVar
+            variables.append(var)
+            image1=PhotoImage(file=f'images/{meals[i]}.png')
+
+            self.check = tk.Checkbutton(self.root, text=f"{meals[i]}", font=('Arial', 16), bg='black', image=image1, variable=var)
+            self.check.grid(row=(i%3), column=(i//3))#removed padding cuz why not
+            self.check.image = image1
+
+
+        '''self.image_1=PhotoImage(file='Chicken Dum Biriyani Small.png')
 
         self.check_1 = tk.Checkbutton(self.root, text="Chicken Dum Biriyani 300", font=('Arial', 16), bg='black', image=self.image_1, variable=self.check_state_1)
         self.check_1.grid(row=0, column=0, padx=45, pady=30)
         self.check_1.image=self.image_1
 
-        self.image_2=PhotoImage(file=r'/Users/brindap/Downloads/Pudina rice Small.png')
+        self.image_2=PhotoImage(file='Pudina rice Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Pudhina Rice 170", font=('Arial', 16), bg='black', image=self.image_2, variable=self.check_state_2)
         self.check.grid(row=0, column=1, padx=30, pady=30)
         self.check.image=self.image_2
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Prawns biriyani Small.png')
+        self.image=PhotoImage(file='Prawns biriyani Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Prawns Biryani,400", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_3)
         self.check.grid(row=0, column=2, padx=45, pady=30)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Veg Thali Small.png')
+        self.image=PhotoImage(file='Veg Thali Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Veg Thali,700", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_4)
         self.check.grid(row=1, column=0, padx=45, pady=20)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Curd rice Small.png')
+        self.image=PhotoImage(file='Curd rice Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Curd Rice,100", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_5)
         self.check.grid(row=1, column=1, padx=30, pady=20)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Plain rice Small.png')
+        self.image=PhotoImage(file='Plain rice Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Plain Rice,80", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_6)
         self.check.grid(row=1, column=2, padx=45, pady=20)
-        self.check.image=self.image
+        self.check.image=self.image'''
 
         self.check = tk.Checkbutton(self.root, text="Confirm selected dishes", font=('Arial', 16), bg='black', fg='white', variable=self.check_state_7)
         self.check.grid( pady=20)
@@ -211,42 +232,53 @@ class mine:
         self.root.title("Starters")
         self.root.configure(bg='black')
         
-        self.check_state_1 = tk.IntVar()
-        self.check_state_2= tk.IntVar()
-        self.check_state_3 = tk.IntVar()
-        self.check_state_4 = tk.IntVar()
-        self.check_state_5 = tk.IntVar()
         self.check_state_6 = tk.IntVar()
+        menu=Menu_Management.Menu_class()
 
-        self.image_1=PhotoImage(file=r'/Users/brindap/Downloads/Hazel Nutella Brownie Small.png')
+
+        desserts = menu.get_starter_items()
+        variables = []
+
+
+        for i in range(len(desserts)):
+            var = tk.IntVar
+            variables.append(var)
+            image1=PhotoImage(file=f'images/{desserts[i]}.png')
+
+            self.check = tk.Checkbutton(self.root, text=f"{desserts[i]}", font=('Arial', 16), bg='black', image=image1, variable=var)
+            self.check.grid(row=(i%3), column=(i//3))#removed padding cuz why not
+            self.check.image = image1
+
+
+        '''self.image_1=PhotoImage(file='Hazel Nutella Brownie Small.png')
 
         self.check_1 = tk.Checkbutton(self.root, text="Hazel Nutella Brownie,96", font=('Arial', 16), bg='black', image=self.image_1, variable=self.check_state_1)
         self.check_1.grid(row=0, column=0, padx=45, pady=30)
         self.check_1.image=self.image_1
 
-        self.image_2=PhotoImage(file=r'/Users/brindap/Downloads/Chocolate Ball Small.png')
+        self.image_2=PhotoImage(file='Chocolate Ball Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Chocolate Ball,30", font=('Arial', 16), bg='black', image=self.image_2, variable=self.check_state_2)
         self.check.grid(row=0, column=1, padx=30, pady=30)
         self.check.image=self.image_2
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Choco Walnut Brownie Small.png')
+        self.image=PhotoImage(file='Choco Walnut Brownie Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Choco Walnut Brownie,60", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_3)
         self.check.grid(row=0, column=2, padx=45, pady=30)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Choco Chip Brownie Small.png')
+        self.image=PhotoImage(file='Choco Chip Brownie Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Choco Chip Brownie,60", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_4)
         self.check.grid(row=1, column=0, padx=90, pady=20)
         self.check.image=self.image
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/Choco Chip Cake Small.png')
+        self.image=PhotoImage(file='Choco Chip Cake Small.png')
 
         self.check = tk.Checkbutton(self.root, text="Choco Chip Cake,540", font=('Arial', 16), bg='black', image=self.image, variable=self.check_state_5)
         self.check.grid(row=1, column=1, padx=90, pady=20)
-        self.check.image=self.image
+        self.check.image=self.image'''
 
         self.check = tk.Checkbutton(self.root, text="Confirm selected dishes", font=('Arial', 16), bg='black', fg='white', variable=self.check_state_6)
         self.check.grid( pady=20)
@@ -268,19 +300,19 @@ class mine:
         self.root.title("Customer")
         self.root.configure(bg='white')
 
-        self.image_1=PhotoImage(file=r'/Users/brindap/Downloads/takeaway Small.png')
+        self.image_1=PhotoImage(file='images/takeaway Small.png')
         
         self.button_takeaway = tk.Button(self.root , text="Take away", font=('Arial', 18), image=self.image_1, command=exit)
         self.button_takeaway.pack()
         self.button_takeaway.image=self.image_1
 
-        self.image_2=PhotoImage(file=r'/Users/brindap/Downloads/dine-in Small.png')
+        self.image_2=PhotoImage(file='images/dine-in Small.png')
 
         self.button_dine = tk.Button(self.root , text="Dine in", font=('Arial', 18),image=self.image_2, command=self.booking_tables)
         self.button_dine.pack()
         self.button_dine.image=self.image_2
 
-        self.image_3=PhotoImage(file=r'/Users/brindap/Downloads/delivery Small.png')
+        self.image_3=PhotoImage(file='images/delivery Small.png')
 
         self.button_delivery = tk.Button(self.root , text="Delivery", font=('Arial', 18),image=self.image_3, command=self.address)
         self.button_delivery.pack()
@@ -298,7 +330,7 @@ class mine:
         self.label = tk.Label(self.root, text="Enter your address", bg='white', fg='black', font=('Courier New Bold', 22))
         self.label.pack(padx=20, pady=20)
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/address Medium.png')
+        self.image=PhotoImage(file='images/address Medium.png')
 
         self.button = tk.Button(self.root , image=self.image)
         self.button.pack()
@@ -322,7 +354,7 @@ class mine:
         self.label = tk.Label(self.root, text="Desired no. of diners", font=('Arial', 18))
         self.label.pack(padx=20, pady=20)
 
-        self.image=PhotoImage(file=r'/Users/brindap/Downloads/tables Medium.png')
+        self.image=PhotoImage(file='images/tables Medium.png')
 
         self.button = tk.Button(self.root , image=self.image)
         self.button.pack()
